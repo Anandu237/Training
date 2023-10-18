19435c5d2771c0fa0366bfd53242373058a7ad77
user <user@DESKTOP-O7TOJMR>
2023-10-18 07:52:35 +0530

    committed

---
+++ src/com/obsqura/numbers/Oddno.java
@@ -0,0 +1,19 @@
+package com.obsqura.numbers;
+
+public class Oddno {
+
+	public static void main(String[] args) {
+
+int a =20;
+
+if(a%2==0){
+System.out.println(" Number is even");
+}
+else {
+System.out.println(" Number is odd");
+}
+}
+
+
+	}
+
---
+++ src/com/obsqura/training/Assignment.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.obsqura.training;
+
+/**
+ * @author user
+ *
+ */
+public class Assignment {
+
+	/**
+	 * @param args
+	 */
+	
+
+			public static void main (String args[]) {
+
+			float a = 10.2f;
+			float b =10.2f;
+
+			float sum = a+b;
+
+			System.out.println ( " sum of a + b  = " + sum );
+
+			}
+			}
+	
+		// TODO Auto-generated method stub
+
